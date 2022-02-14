10cf03004cc83f1c1b11b80774a066eb4f2bbcf9
Samuel Culpepper <samuel@samuelculpepper.com>
2022-02-14 18:08:45 +0100

    qz: configs: add plantuml

--- rde/examples/abcdw/configs.scm
+++ rde/examples/abcdw/configs.scm
@@ -419,6 +419,7 @@
            "emacs-forge"
            "emacs-debbugs"
            "emacs-ob-async"
+           "emacs-plantuml-mode"
            "emacs-org-fragtog"
 
            "emacs-explain-pause-mode"
