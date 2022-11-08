6be418de8cc457466a949931919cf873d3b97659
Samuel Culpepper <samuel@samuelculpepper.com>
2022-11-08 12:19:51 +0100

    rde: linux: Disable noise-cancellation

--- rde/features/linux.scm
+++ rde/features/linux.scm
@@ -106,46 +106,46 @@ ctl_type.pipewire {
 ")))))
 
      ;; pipewire modules :: https://docs.pipewire.org/page_pipewire_modules.html
-     (simple-service
-      'pipewire-add-input-denoising
-      home-xdg-configuration-files-service-type
-      `(("pipewire/pipewire.conf.d/99-input-denoising.conf"
-         ,(mixed-text-file
-           "99-input-denoising.conf"
-           #~(string-append "
-context.modules = [
-  {   name = libpipewire-module-filter-chain
-      args = {
-          node.description =  \"Noise Canceling source\"
-          media.name =  \"Noise Canceling source\"
-          filter.graph = {
-              nodes = [
-                  {
-                      type = ladspa
-                      name = rnnoise
-                      plugin = " #$(file-append noise-suppression-for-voice "/lib/ladspa/librnnoise_ladspa.so") "
-                      label = noise_suppressor_mono
-                      control = {
-                          \"VAD Threshold (%)\" 50.0
-                          \"VAD Grace Period (ms)\" 200
-                          \"Retroactive VAD Grace (ms)\" 0
-                      }
-                  }
-              ]
-          }
-          capture.props = {
-              node.name =  \"capture.rnnoise_source\"
-              node.passive = true
-              audio.rate = 48000
-          }
-          playback.props = {
-              node.name =  \"rnnoise_source\"
-              media.class = Audio/Source
-              audio.rate = 48000
-          }
-      }
-  }
-]")))))
+;;      (simple-service
+;;       'pipewire-add-input-denoising
+;;       home-xdg-configuration-files-service-type
+;;       `(("pipewire/pipewire.conf.d/99-input-denoising.conf"
+;;          ,(mixed-text-file
+;;            "99-input-denoising.conf"
+;;            #~(string-append "
+;; context.modules = [
+;;   {   name = libpipewire-module-filter-chain
+;;       args = {
+;;           node.description =  \"Noise Canceling source\"
+;;           media.name =  \"Noise Canceling source\"
+;;           filter.graph = {
+;;               nodes = [
+;;                   {
+;;                       type = ladspa
+;;                       name = rnnoise
+;;                       plugin = " #$(file-append noise-suppression-for-voice "/lib/ladspa/librnnoise_ladspa.so") "
+;;                       label = noise_suppressor_mono
+;;                       control = {
+;;                           \"VAD Threshold (%)\" 50.0
+;;                           \"VAD Grace Period (ms)\" 200
+;;                           \"Retroactive VAD Grace (ms)\" 0
+;;                       }
+;;                   }
+;;               ]
+;;           }
+;;           capture.props = {
+;;               node.name =  \"capture.rnnoise_source\"
+;;               node.passive = true
+;;               audio.rate = 48000
+;;           }
+;;           playback.props = {
+;;               node.name =  \"rnnoise_source\"
+;;               media.class = Audio/Source
+;;               audio.rate = 48000
+;;           }
+;;       }
+;;   }
+;; ]")))))
 
      (simple-service
       'pipewire-add-shepherd-daemons
